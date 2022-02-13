77ded61759be62f9f7e60a478f33077dfe7339ab
SWillames <sergiowoc@gmail.com>
2022-02-13 20:03:28 -0300

    project created.

---
+++ src/test/java/sergiowill/api_beer/ApiBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package sergiowill.api_beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
